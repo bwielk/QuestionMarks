a651500b1a1361827be4640e4391707cf9dc656d
bwielk <bwielk@gmail.com>
2018-01-22 23:48:03 +0100

    Separating methods

--- src/questionmarks/QuestionMarksChecker.java
+++ src/questionmarks/QuestionMarksChecker.java
@@ -5,17 +5,28 @@ import java.util.ArrayList;
 public class QuestionMarksChecker {
 	
 	private boolean result = false;
+	ArrayList<String> characters = new ArrayList<String>();
+	ArrayList<Integer> indexes = new ArrayList<Integer>();
 	
 	public boolean run(String s){
-		ArrayList<String> characters = new ArrayList<String>();
-		ArrayList<Integer> indexes = new ArrayList<Integer>();
-		//populates the numbers array
-		char[] numbers = new char[10];
-		for(int i = 0; i<10; i++){
-			numbers[i] = (char)(i + '0'); 
-		}
-		//Extracts numbers from the String and gets their index
+		char[] numbers = createArrayOfDigits(10);
 		char[] chars = s.toCharArray();
+		findNumbers(chars, numbers);
+		return checkForQuestionMarks(chars);
+	}
+	
+	public char[] createArrayOfDigits(int limit){
+		if(limit <= 10){
+			char[] numbers = new char[limit];
+			for(int i = 0; i<10; i++){
+				numbers[i] = (char)(i + '0'); 
+				return numbers;
+			}
+		}
+		return null;
+	}
+	
+	public void findNumbers(char[] chars, char[] numbers){
 		for(int i = 0; i< chars.length; i++){
 			char ch = chars[i];
 			for(int n = 0; n<numbers.length; n++){
@@ -25,23 +36,25 @@ public class QuestionMarksChecker {
 				}
 			}
 		}
-		//Checks if any of pairs is equal to 10
+	}
+	
+	public boolean checkForQuestionMarks(char[] stringToSearchIn){
+		boolean result = true;
 		for(int i = 0; i<characters.size(); i++){
 			if(Integer.valueOf(characters.get(i)) + Integer.valueOf(characters.get(i)) == 10){
 				int count = 0;
 				for(int x = indexes.get(i); x<indexes.get(i+1); x++){
-					if(chars[x] == '?'){
+					if(stringToSearchIn[x] == '?'){
 						count ++;
 					}
 				}
 				if(count == 3){
-					return true;
+					result =  true;
 				}else{
-					return false;
+					result =  false;
 				}
 			}
 		}
 		return result;
 	}
-
 }
